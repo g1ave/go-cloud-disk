2d4bd606d23dbc8b007a801093463bb68703798c
g1ave <qw112657@gmail.com>
2022-07-07 17:44:05 +0800

    test: add database connection test

--- tests/model_test.go
+++ tests/model_test.go
@@ -1,7 +1,55 @@
 package tests
 
-import "testing"
+import (
+	"flag"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	"github.com/zeromicro/go-zero/rest"
+	"gopkg.in/yaml.v3"
+	"io/ioutil"
+	"testing"
+)
 
-func DataBaseConnTest(t *testing.T) {
+type Config struct {
+	rest.RestConf `yaml:",inline"`
 
+	Database struct {
+		DSN string `yaml:"dsn"`
+	}
+}
+
+var configFile = flag.String("f", "../core/etc/core-api.yaml", "the config file")
+
+func TestDataBaseConn(t *testing.T) {
+	yamlFile, err := ioutil.ReadFile(*configFile)
+	if err != nil {
+		t.Errorf("read file error: %v", err)
+	}
+	config := &Config{}
+	err = yaml.Unmarshal(yamlFile, config)
+
+	if err != nil {
+		t.Errorf("yaml unmarshal error: %v", err)
+		return
+	}
+	dataSource := config.Database.DSN
+	_, err = gorm.Open("mysql", dataSource)
+	if err != nil {
+		t.Errorf("connect DB error: %v, %v", err, dataSource)
+		return
+	}
+}
+
+func TestReadYamlFile(t *testing.T) {
+	type Database struct {
+		DSN string
+	}
+	db := &Database{}
+	err := yaml.Unmarshal([]byte("dsn: test"), db)
+	if err != nil {
+		t.Errorf("err: %v", err)
+		return
+	}
+	fmt.Println(db.DSN)
 }
